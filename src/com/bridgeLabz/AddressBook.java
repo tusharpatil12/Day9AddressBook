10af1d6d6d8cc38af57335dfd46a47b63611c2fc
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 <tusharpatil22700@gmail.com>
2022-10-28 09:21:16 -0700

    initial commit

--- src/com/bridgeLabz/AddressBook.java
+++ src/com/bridgeLabz/AddressBook.java
@@ -1,9 +1,38 @@
 package com.bridgeLabz;
 
+import java.util.Scanner;
+
 public class AddressBook {
 
 	public static void main(String[] args) {
 		 System.out.println("Welcome to the Address Book program");
+		 
+		  String First_Name;
+	        String Last_Name;
+	        String Address;
+	        String City_Name;
+	        String State_Name;
+	        int Zip_Code;
+	        double Phone_Number;
+	        String Email_Id;
+
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter First Name");
+	        First_Name = sc.nextLine();
+	        System.out.println("Enter Last Name");
+	        Last_Name = sc.nextLine();
+	        System.out.println("Enter Address");
+	        Address = sc.nextLine();
+	        System.out.println("Enter City Name");
+	        City_Name = sc.nextLine();
+	        System.out.println("Enter State Name");
+	        State_Name = sc.nextLine();
+	        System.out.println("Enter Zip Code");
+	        Zip_Code = sc.nextInt();
+	        System.out.println("Enter Phone Number");
+	        Phone_Number = sc.nextDouble();
+	        System.out.println("Enter Email Id");
+	        Email_Id = sc.next();
 	}
 
 }
