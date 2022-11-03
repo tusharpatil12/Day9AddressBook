32e1bf1993a38442709bbdbbd3161fff5d10663b
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 <tusharpatil22700@gmail.com>
2022-11-02 20:46:20 -0700

    welcome

--- src/com/bridgeLabz/AddressBook.java
+++ src/com/bridgeLabz/AddressBook.java
@@ -4,41 +4,45 @@ import java.util.Scanner;
 
 public class AddressBook {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Address Book program");
-		AddressBook ad = new AddressBook();
-		ad.addContact();
-	}
-	public void addContact() { 
+	 static Scanner sc = new Scanner(System.in);
+	    static PersonContact cp = new PersonContact();
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to the Address Book program");
 
-		String First_Name;
-		String Last_Name;
-		String Address;
-		String City_Name;
-		String State_Name;
-		int Zip_Code;
-		double Phone_Number;
-		String Email_Id;
+	        AddressBook ad = new AddressBook();
+	        ad.addContact();
+	    }
 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter First Name");
-		First_Name = sc.nextLine();
-		System.out.println("Enter Last Name");
-		Last_Name = sc.nextLine();
-		System.out.println("Enter Address");
-		Address = sc.nextLine();
-		System.out.println("Enter City Name");
-		City_Name = sc.nextLine();
-		System.out.println("Enter State Name");
-		State_Name = sc.nextLine();
-		System.out.println("Enter Zip Code");
-		Zip_Code = sc.nextInt();
-		System.out.println("Enter Phone Number");
-		Phone_Number = sc.nextDouble();
-		System.out.println("Enter Email Id");
-		Email_Id = sc.next();
-		
-		 PersonContact cp = new PersonContact();
+	    public void addContact() {
+
+//	        String First_Name;
+//	        String Last_Name;
+//	        String Address;
+//	        String City_Name;
+//	        String State_Name;
+//	        int Zip_Code;
+//	        double Phone_Number;
+//	        String Email_Id;
+
+
+	        System.out.println("Enter First Name");
+	        String First_Name = sc.nextLine();
+	        System.out.println("Enter Last Name");
+	        String Last_Name = sc.nextLine();
+	        System.out.println("Enter Address");
+	        String Address = sc.nextLine();
+	        System.out.println("Enter City Name");
+	        String City_Name = sc.nextLine();
+	        System.out.println("Enter State Name");
+	        String State_Name = sc.nextLine();
+	        System.out.println("Enter Zip Code");
+	        int Zip_Code = sc.nextInt();
+	        System.out.println("Enter Phone Number");
+	        double Phone_Number = sc.nextDouble();
+	        System.out.println("Enter Email Id");
+	        String Email_Id = sc.next();
+
+
 	        cp.setFirst_Name(First_Name);
 	        cp.setLast_Name(Last_Name);
 	        cp.setAddress(Address);
@@ -47,14 +51,93 @@ public class AddressBook {
 	        cp.setZip_Code(Zip_Code);
 	        cp.setPhone_Number(Phone_Number);
 	        cp.setEmail_Id(Email_Id);
-	        
-	        displyContact(cp);
-	        sc.close();
+
+	        displayContact(cp);
+	        editContact(cp);
 
 	    }
-	    public void displyContact(PersonContact contact){
-	        System.out.println(contact);
 
-	}
+	    public void displayContact(PersonContact cp) {
+	        System.out.println(cp);
 
+	    }
+
+	    public void editContact(PersonContact cp) {
+	        int choice = 0;
+	        while (choice < 1 || choice > 8) {
+	            System.out.println("\nWhat would you like to update?");
+	            System.out.println("1. Name");
+	            System.out.println("2. Last Name");
+	            System.out.println("3. Address");
+	            System.out.println("4. City Name");
+	            System.out.println("5. State Name");
+	            System.out.println("6. Zip Code");
+	            System.out.println("7. Phone Number");
+	            System.out.println("8. Email Id");
+	            System.out.print("\nEnter your choice : ");
+	            choice = sc.nextInt();
+
+	            if (!(choice >=1 && choice <= 8))
+	                System.out.println("\nInvalid choice!\nPlease try again.\n");
+	        }
+
+	        switch (choice) {
+	            case 1 :
+	                System.out.print("Enter the updated First Name :	");
+	                String First_Name = sc.next();
+	                cp.setFirst_Name(First_Name);
+	                break;
+	            case 2 :
+	                System.out.print("Enter the updated Last Name :	");
+	                String Last_Name = sc.next();
+	                cp.setLast_Name(Last_Name);
+	                break;
+	            case 3 :
+	                System.out.print("Enter the updated Address :	");
+	                String Address = sc.nextLine();
+	                cp.setAddress(Address);
+	                break;
+	            case 4 :
+	                System.out.print("Enter the updated City :	");
+	                String City_Name = sc.next();
+	                cp.setCity_Name(City_Name);
+	                break;
+	            case 5 :
+	                System.out.print("Enter the updated State :	");
+	                String State_Name = sc.next();
+	                cp.setState_Name(State_Name);
+	                break;
+	            case 6 :
+	                System.out.print("Enter the updated zip :	");
+	                int Zip_Code = sc.nextInt();
+	                cp.setZip_Code(Zip_Code);
+	                break;
+	            case 7 :
+	                System.out.print("Enter the updated Phone Number :	");
+	                double Phone_Number = sc.nextDouble();
+	                cp.setPhone_Number(Phone_Number);
+	                break;
+	            case 8 :
+	                System.out.print("Enter the updated Email Address :	");
+	                String Email_Id = sc.next();
+	                cp.setEmail_Id(Email_Id);
+	                break;
+	        }
+
+	        System.out.println("\nIs there anything else you'd like to update?");
+	        System.out.print("Enter 'Yes' or 'No' : ");
+	        char continueEdit = sc.next().charAt(0);
+	        if (continueEdit == 'Y' || continueEdit == 'y') {
+	            editContact(cp);
+
+	        }
+	        else if (continueEdit == 'N' || continueEdit == 'n') {
+	            System.out.println("\n\nHere is the updated Address Book.");
+	            displayContact(cp);
+	        }
+	        else {
+	            System.out.println("\nInvalid Input.\nPlease try again!");
+	        }
+	    }
+	    
 }
